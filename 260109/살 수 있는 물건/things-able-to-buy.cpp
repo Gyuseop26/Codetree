01946c14a4d20a53e080b94f919b9b6ea129bd71
정규섭 <jrb9663@hanyang.ac.kr>
2026-01-09 15:14:57 +0900

    Title: 살 수 있는 물건, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260109/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int money;
+
+    cin >> money;
+
+    if (money >= 3000){
+        cout << "book";
+    }
+    
+    else if (money >= 1000 && money < 3000){
+        cout << "mask";
+    }
+
+    else{
+        cout << "no";
+    }
+
+    return 0;
+}
