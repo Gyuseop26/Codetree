ec200e3985cc018b3940d4f790884aca59e5da2e
정규섭 <jrb9663@hanyang.ac.kr>
2026-01-12 22:29:31 +0900

    Title: 일의 자리 배열, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260112/일의 자리 배열/array-with-units.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[10];
+
+    cin >> a[0] >> a[1];
+
+    for (int i = 2; i < 10 ; i++){
+        a[i] = a[i - 1] + a[i -2 ];
+        if (a[i] >= 10) a[i] -= 10;
+    }
+    for (int i = 0; i < 10; i++){
+        cout << a[i] << " ";
+    }
+    return 0;
+}
