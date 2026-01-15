bcf7b95679722ec7d71448f2e36ba104427567ce
정규섭 <jrb9663@hanyang.ac.kr>
2026-01-16 02:21:40 +0900

    Title: 짝수의 합 n번 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260116/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.cpp
+++ 260116/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.cpp
@@ -2,17 +2,16 @@
 using namespace std;
 
 int main() {
-    int n, a, b, sum = 0;
+    int n, a, b, sum;
     cin >> n;
     for (int i = 0; i < n; i++){
         cin >> a >> b;
-        
+        sum = 0;
         for (int j = a; j <=b ; j++){
             if (!(j % 2 )) sum += j;
         }
         
         cout << sum << "\n";
-        sum = 0;
     }
     
     return 0;
