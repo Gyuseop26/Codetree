239fbe80cee270dd5d81bbe03364fc212ee68a18
정규섭 <jrb9663@hanyang.ac.kr>
2026-01-18 00:18:42 +0900

    Title: 다양하게 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260118/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << fixed;
+    cout.precision(10);
+
+    cout << "Total days in Year" << endl << 365 << endl << "Circumference rate" << endl;
+    cout << double(3.1415926535);
+    return 0;
+}
