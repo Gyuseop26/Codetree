249ecfe201b5dcd03256a2ccc66e4c2c74b368c2
정규섭 <jrb9663@hanyang.ac.kr>
2026-01-17 21:41:02 +0900

    Title: 두 배열의 곱, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 260117/두 배열의 곱/multiple-of-two-arrays.cpp
+++ 260117/두 배열의 곱/multiple-of-two-arrays.cpp
@@ -12,14 +12,10 @@ int main() {
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
             cin >> arr2[i][j];
-        }
-    }
-
-    for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; j++){
             cout << arr1[i][j] * arr2[i][j] << " ";
         }
         cout << "\n";
     }
+
     return 0;
 }
