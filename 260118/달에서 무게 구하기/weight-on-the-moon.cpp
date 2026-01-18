62efeab46565b653f039dafb27969ea002bb0b37
정규섭 <jrb9663@hanyang.ac.kr>
2026-01-18 13:41:12 +0900

    Title: 달에서 무게 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260118/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << fixed;
+    int weight = 13;
+	double moon_g = 0.165;
+
+	cout.precision(6);
+	cout <<weight << " * " << moon_g << " = " << weight * moon_g ;
+
+	return 0;
+
+}
+
+
