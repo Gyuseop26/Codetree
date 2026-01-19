cef07ab57920802c78126201cacb786dc4449792
정규섭 <jrb9663@hanyang.ac.kr>
2026-01-19 13:05:06 +0900

    Title: 변수 값 교체하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260119/변수 값 교체하기 3/replacing-variable-values-3.cpp
+++ 260119/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
     int a = 3, b = 5;
-    cout << b << "\n" <<a;
+    
+	int temp = a;
+	a = b;
+	b = temp;
+
+    cout << a << "\n" << b ;
     return 0;
 }
+
