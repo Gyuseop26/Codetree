c657a54eedea9010be99fd9d7040334c56508c51
정규섭 <jrb9663@hanyang.ac.kr>
2026-01-21 22:31:21 +0900

    Title: 시력 검사 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260121/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	double a;
+
+	cin >> a;
+
+	if (a >= 1.0) {
+		cout << "High" << endl;
+	}
+	else if (a >= 0.5) {
+		cout << "Middle" << endl;
+	}
+
+	else cout << "Low";
+	return 0;
+
+}
+
+
