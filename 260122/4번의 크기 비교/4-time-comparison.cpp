3a55a588e9b5d7db06ea2f6f256f47fb318cada8
정규섭 <jrb9663@hanyang.ac.kr>
2026-01-22 23:05:16 +0900

    Title: 4번의 크기 비교, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260122/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    
+    a > b ? cout << "1\n" : cout << "0\n";
+    a > c ? cout << "1\n" : cout << "0\n";
+    a > d ? cout << "1\n" : cout << "0\n";
+    a > e ? cout << "1" : cout << "0";
+    return 0;
+}
