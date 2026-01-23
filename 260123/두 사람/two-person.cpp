b9e90f4dea71bc952233f21d8c378ff694e63015
정규섭 <jrb9663@hanyang.ac.kr>
2026-01-23 18:19:17 +0900

    Title: 두 사람, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260123/두 사람/two-person.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age_1, age_2;
+    char gender_1, gender_2;
+
+    cin >> age_1 >> gender_1 >> age_2 >> gender_2;
+
+    if (age_1 >= 10 || age_2 >= 19){
+        if (gender_1 == 'M' || gender_2 == 'M') cout << 1;
+        else cout << 0;
+    }
+    else cout << 0;
+    
+    return 0;
+}
