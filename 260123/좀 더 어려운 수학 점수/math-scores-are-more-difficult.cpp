bfe1152bde59d403a85957677ec3fdf8bf0a7d36
정규섭 <jrb9663@hanyang.ac.kr>
2026-01-23 17:43:59 +0900

    Title: 좀 더 어려운 수학 점수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260123/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A_math, A_eng, B_math, B_eng;
+    cin >> A_math >> A_eng >> B_math >> B_eng;
+
+    if(A_math == B_math){
+        if (A_eng > B_eng) cout << "A";
+        else cout << "B";
+    }
+    else if (A_math > B_math) cout << "A";
+   
+    else cout << "B";
+
+    return 0;
+}
