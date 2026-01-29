b59087af25d1c1c19459906f223410c911b364e0
정규섭 <jrb9663@hanyang.ac.kr>
2026-01-29 14:04:51 +0900

    Title: 100점 도달하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260129/100점 도달하기/reach-100-point.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    while (N <= 100){
+        
+        if (N >= 90) cout << 'A' << ' ';
+        else if (N >= 80) cout << 'B' << ' ';
+        else if (N >= 70) cout << 'C' << ' ';
+        else if (N >= 60) cout << 'D' << ' ';
+        else cout << 'F' << ' ';
+        N++;
+    }
+    return 0;
+}
