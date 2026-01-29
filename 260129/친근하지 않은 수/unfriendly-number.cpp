2f66830a426a41e984c7beb344d72e8417b387dc
정규섭 <jrb9663@hanyang.ac.kr>
2026-01-29 23:30:57 +0900

    Title: 친근하지 않은 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260129/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++){
+        if((i%2) && (i%3) && (i%5)) cnt ++;
+        
+    }
+    cout << cnt;
+    return 0;
+}
