2599a8f52bf4fe69519860cb7f90f34d6ec8e991
정규섭 <jrb9663@hanyang.ac.kr>
2026-01-30 10:46:39 +0900

    Title: 특정 수 맞히기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260130/특정 수 맞히기/catching-specific-number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    while(true){
+        cin >> n;
+        
+        if (n == 25) {
+            cout << "Good";
+            break;
+        }
+
+        else if (n < 25) cout << "Higher\n";
+
+        else if (n > 25) cout << "Lower\n";
+        
+    }
+    return 0;
+}
