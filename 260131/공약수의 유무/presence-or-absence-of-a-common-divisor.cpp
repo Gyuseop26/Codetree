db3413ec132a53575ad7b3a4092e7b3eb865284b
정규섭 <jrb9663@hanyang.ac.kr>
2026-01-31 15:30:11 +0900

    Title: 공약수의 유무, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260131/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
+++ 260131/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main() {
-    // Please write your code here.
+    int a, b;
+    bool flag = false;
+
+    cin >> a >> b;
+    for (int i = a; i <= b; i++){
+        if(!(1920 % i) && !(2880 % i)){
+            flag = true;
+            break;
+        }
+    }
+    cout << (flag ? 1 : 0);
     return 0;
 }
