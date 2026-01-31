842500c2074dcdfc42a76653d03c62eee8231ee5
정규섭 <jrb9663@hanyang.ac.kr>
2026-01-31 16:42:16 +0900

    Title: 모두 3의 배수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260131/모두 3의 배수/all-multiples-of-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    bool flag = false;
+
+    for (int i = 0; i < 5; i++){
+        cin >> n;
+        if(n%3){
+            flag = true;
+            break;
+        }
+    }
+    cout << (flag ? 0 : 1); 
+    return 0;
+}
