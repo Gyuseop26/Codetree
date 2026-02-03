6e646f64f651fd41579d43f43a278875a5f56690
정규섭 <jrb9663@hanyang.ac.kr>
2026-02-03 16:12:23 +0900

    Title: 별표 출력하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260203/별표 출력하기 3/print-star-3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        
+        for (int j = 0; j < 2 * i; j++) cout << ' ';
+
+        for (int j = 0; j < 2*n - 1 - (2*i); j++) {
+            cout << "*";
+            if (j < 2 * n - 2 - (2 * i)) cout << ' '; // 각 행에 마지막 별 출력시 공백 출력 X
+        }
+
+        cout << "\n";
+    }
+    return 0;
+}
