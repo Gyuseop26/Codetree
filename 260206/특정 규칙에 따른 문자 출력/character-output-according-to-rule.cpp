ee607b1d0715a832a0294216596feb8b5435a858
정규섭 <jrb9663@hanyang.ac.kr>
2026-02-06 12:29:39 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260206/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < 2 * n - 1; i++){
+        int dist = abs(n-1-i);
+        string empty = string(2 * dist, ' ');
+
+        if (i < n-1) cout << empty;
+
+        for (int j = 0; j < n - dist; j++)
+            cout << "@" << (j == n - dist -1 ? "" : " ");
+
+
+        if (i >= n-1) cout << empty;
+
+        cout << "\n";
+    }
+    return 0;
+}
