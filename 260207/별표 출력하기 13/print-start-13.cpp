9be2bbe5ef4752df2b8673f2aa86da8512b32a3b
정규섭 <jrb9663@hanyang.ac.kr>
2026-02-07 23:27:57 +0900

    Title: 별표 출력하기 13, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260207/별표 출력하기 13/print-start-13.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < 2 * n; i++){
+        // k를 반사 인덱스라고 생각하기, 전체는 2n-1 행을 갖지만, k는 n개까지만 생각하기
+        // i가 n보다 작으면 k는 i 그대로 따라가고, 커지면 반사되는 인덱스 따라가기
+        int k = (i < n ? i : (2 * n - 1 - i));
+        
+        int star = (k % 2 != 0 ? (k/2 + 1) : (n - k/2));
+        
+        for (int j = 0; j < star; j++){
+            cout << '*' << (j == star - 1 ? "" : " ");
+        }
+        cout << "\n";
+    }
+    return 0;
+}
