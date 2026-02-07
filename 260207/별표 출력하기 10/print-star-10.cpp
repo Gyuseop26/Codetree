6acf20375b219e96b24c984d7cca5c97a982057d
정규섭 <jrb9663@hanyang.ac.kr>
2026-02-07 22:15:21 +0900

    Title: 별표 출력하기 10, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260207/별표 출력하기 10/print-star-10.cpp
+++ 260207/별표 출력하기 10/print-star-10.cpp
@@ -9,19 +9,26 @@ int main() {
         // i가 n보다 작으면 k는 i 그대로 따라가고, 커지면 반사되는 인덱스 따라가기
         int k = (i < n ? i : (2 * n - 1 - i));
         
-        if (!(k%2)){
-            for (int j = 0; j < k/2 + 1; j++){
-                cout << '*' << (j == k/2 ? "": " ");
-            }
+        int star = (k % 2 == 0 ? (k/2 + 1) : (n - k/2));
+        
+        for (int j = 0; j < star; j++){
+            cout << '*' << (j == star - 1 ? "" : " ");
         }
 
-        else {
-            for (int j = 0; j < n - k/2; j++){
-                cout << '*' << (j == n - k/2 - 1 ? "": " ");
-            }
-        }
         cout << "\n";
+        
+        // if (!(k%2)){
+        //     for (int j = 0; j < k/2 + 1; j++){
+        //         cout << '*' << (j == k/2 ? "": " ");
+        //     }
+        // }
 
+        // else {
+        //     for (int j = 0; j < n - k/2; j++){
+        //         cout << '*' << (j == n - k/2 - 1 ? "": " ");
+        //     }
+        // }
+        // cout << "\n";
 
     }
     return 0;
