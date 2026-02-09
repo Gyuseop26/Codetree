01f726ad3da04756f057fe349a06c4b74fb150ef
정규섭 <jrb9663@hanyang.ac.kr>
2026-02-09 14:43:37 +0900

    Title: 규칙을 찾아 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260209/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
+++ 260209/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
@@ -6,13 +6,11 @@ int main() {
     cin >> n;
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
-            if (i == 0 || i == n-1 || j == 0 || j == n-1) cout <<'*';
+            if (i == 0 || i > j || j == n-1) cout <<'*';
 
-            else if (i - j > 0) cout << '*';
-
-            else cout <<' ';
+            else cout << ' ';
             
-            if (j < n-1) cout <<' ';
+            if (j < n-1) cout << ' ';
         }
         cout << "\n";
     }
