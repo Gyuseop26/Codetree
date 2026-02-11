0b3da7741a42e9019f84fc7604aeeac4efa7b913
정규섭 <jrb9663@hanyang.ac.kr>
2026-02-11 22:43:27 +0900

    Title: 좌우반전시킨 정사각형, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260211/좌우반전시킨 정사각형/left-right-inverted-square.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++){
+        for (int j = n * i; j >= i; j -= i) cout << j << (j != i ? " " : "");
+        cout << "\n";
+    }
+    return 0;
+}
