0a27ade5e61b56098280aeded60c83e9408c2d1d
정규섭 <jrb9663@hanyang.ac.kr>
2026-02-13 01:53:03 +0900

    Title: 좌우 지그재그 출력, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 260213/좌우 지그재그 출력/print-lr-zigzag.cpp
+++ 260213/좌우 지그재그 출력/print-lr-zigzag.cpp
@@ -6,7 +6,7 @@ int main() {
     cin >> n;
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
-            int mask_j = (i%2 == 0) ? j : (n - 1 - j);
+            int mask_j = (i%2 == 0) ? j : (n - 1 - j); // 행에 따른 반전된 j값 사용
             
             cout << n * i + mask_j + 1 << (j == n-1 ? "" : " ");
         }
