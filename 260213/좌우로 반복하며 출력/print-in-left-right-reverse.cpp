21fa54f778bc3e5627318250671d59888f4ee0e6
정규섭 <jrb9663@hanyang.ac.kr>
2026-02-13 00:48:31 +0900

    Title: 좌우로 반복하며 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260213/좌우로 반복하며 출력/print-in-left-right-reverse.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        if (!(i%2)){
+            for (int j = 1; j <= n; j++) cout << j;
+        }
+        else {
+            for (int j = n; j >= 1; j--) cout << j;
+        }
+        cout <<"\n";
+    }
+    return 0;
+}
