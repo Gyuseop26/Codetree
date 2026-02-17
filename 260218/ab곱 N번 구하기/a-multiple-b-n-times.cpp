35b78a34dd3481834e9b20e6c1b489ecffd2c7b0
정규섭 <jrb9663@hanyang.ac.kr>
2026-02-18 00:53:55 +0900

    Title: ab곱 N번 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260218/ab곱 N번 구하기/a-multiple-b-n-times.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        int a, b, mul = 1;
+        cin >> a >> b;
+        
+        for (int j = a; j <= b; j++){
+            mul *= j;
+        }
+        cout << mul << "\n";
+    }
+    return 0;
+}
