b9f8526462541d8ae98a26fa0b2825f9b9980a37
정규섭 <jrb9663@hanyang.ac.kr>
2026-03-06 01:12:49 +0900

    Title: 평균점수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260306/평균점수 구하기/print-average.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double score, sum = 0;
+
+    for (int i = 0; i < 8; i++){
+        cin >> score;
+        sum += score;
+    }
+    cout << fixed;
+    cout.precision(1);
+    cout << sum / 8;
+    return 0;
+}
