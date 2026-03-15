784001b546ced0b4e29d4b23a882e664d69a3925
정규섭 <jrb9663@hanyang.ac.kr>
2026-03-15 18:00:25 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260315/특정 조건에 맞게 출력하기/print-in-specific-conditions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, arr[100], cnt = 0;
+    
+    for (int i = 0; i < 100; i++){
+        cin >> arr[i];
+        
+        if (arr[i] % 2 == 0) arr[i] /=2;
+        else arr[i] += 3;
+        
+        if (arr[i] == 0) break;
+
+        cout << arr[i] << ' ';
+    }
+    return 0;
+}
