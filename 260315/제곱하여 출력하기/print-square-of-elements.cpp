421b3a4480db888ee81891f99b0e1e462a218ab1
정규섭 <jrb9663@hanyang.ac.kr>
2026-03-15 16:02:27 +0900

    Title: 제곱하여 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260315/제곱하여 출력하기/print-square-of-elements.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, num;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        cin >> num;
+        cout << num * num << ' ';
+    }
+    return 0;
+}
