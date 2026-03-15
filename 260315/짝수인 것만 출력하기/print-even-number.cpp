48dad9b2ce37a6162887cae04d61e9dce74c7ee4
정규섭 <jrb9663@hanyang.ac.kr>
2026-03-15 17:01:03 +0900

    Title: 짝수인 것만 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260315/짝수인 것만 출력하기/print-even-number.cpp
+++ 260315/짝수인 것만 출력하기/print-even-number.cpp
@@ -2,12 +2,22 @@
 using namespace std;
 
 int main() {
-    int n, num;
+    int n, num, cnt = 0;
     cin >> n;
+
+    int arr[n];
 
     for (int i = 0; i < n; i++){
         cin >> num;
-        if (num % 2 ==0) cout << num << (i < n-1 ? " " : "");
+        
+        if (num % 2 ==0) {
+            arr[cnt] = num;
+            cnt++;
+        }
+    }
+
+    for (int i = 0; i < cnt; i++){
+        cout << arr[i] << (i < cnt - 1 ? " ": "");
     }
     return 0;
 }
