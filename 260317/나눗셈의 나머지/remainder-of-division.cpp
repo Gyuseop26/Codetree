9d3cee3afd502afe0a29a29191941d66672e63b7
정규섭 <jrb9663@hanyang.ac.kr>
2026-03-17 14:17:18 +0900

    Title: 나눗셈의 나머지, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260317/나눗셈의 나머지/remainder-of-division.cpp
+++ 260317/나눗셈의 나머지/remainder-of-division.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 int main() {
-    int count_arr[10] ={0}, sum = 0, A, B;
+    int count_arr[1000] = {0}, sum = 0, A, B;
     
     cin >> A >> B;
 
-    while(true){
-        A /= B;
+    while(A > 1){
         count_arr[A % B]++;
-
-        if (A <= 1) break;
+        A /= B;
     }
 
-    for (int i = 0; i < 10; i++){
-        sum += count_arr[i] * count_arr[i];
+    for (int i = 0; i < 1000; i++){
+        if (count_arr[i] > 0){
+            sum += count_arr[i] * count_arr[i];
+        }
     }
 
     cout << sum;
